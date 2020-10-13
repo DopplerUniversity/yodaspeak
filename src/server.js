cda2c65bed839b0d3745f4de03bc72fc6e2e31e2
Ryan Blunden <ryan.blunden@gmail.com>
2020-10-13 12:26:46 +1000

    Define defaults for hostname and port and capture SIGTERM

--- src/server.js
+++ src/server.js
@@ -6,13 +6,22 @@ import app from './app.js'
 ;(async () => {
     let httpServer, httpsServer
     const appConfig = await config.fetch()
+    const HOST = appConfig.HOSTNAME || '0.0.0.0'
+    const PORT = appConfig.PORT || 3000
 
-    httpServer = http.createServer(app).listen(appConfig.PORT, appConfig.HOSTNAME, () => {
-        console.log(
-            colors.green(
-                `[info]: HTTP server at http://${appConfig.HOSTNAME}:${appConfig.PORT}/ (Press CTRL+C to quit)`
-            )
-        )
+    const onShutdown = code => {
+        console.log(colors.green(`\n[info]: Recieved "${code}"`))
+        console.log(colors.green(`[info]: Shutting down HTTP server`))
+        httpServer.close()
+
+        if (httpsServer) {
+            console.log(colors.green(`[info]: Shutting down HTTPS server`))
+            httpsServer.close()
+        }
+    }
+
+    httpServer = http.createServer(app).listen(PORT, HOST, () => {
+        console.log(colors.green(`[info]: HTTP server at http://${HOST}:${PORT}/ (Press CTRL+C to quit)`))
     })
 
     if (appConfig.TLS_PORT && appConfig.TLS_CERT) {
@@ -24,22 +33,15 @@ import app from './app.js'
                 },
                 app
             )
-            .listen(appConfig.TLS_PORT, appConfig.HOSTNAME, () => {
+            .listen(appConfig.TLS_PORT, HOST, () => {
                 console.log(
                     colors.green(
-                        `[info]: HTTPS server at https://${appConfig.HOSTNAME}:${appConfig.TLS_PORT}/ (Press CTRL+C to quit)`
+                        `[info]: HTTPS server at https://${HOST}:${appConfig.TLS_PORT}/ (Press CTRL+C to quit)`
                     )
                 )
             })
     }
 
-    process.on('SIGINT', () => {
-        console.log(colors.green(`\n[info]: Shutting down HTTP server`))
-        httpServer.close()
-
-        if (httpsServer) {
-            console.log(colors.green(`[info]: Shutting down HTTPS server`))
-            httpsServer.close()
-        }
-    })
+    process.on('SIGINT', onShutdown)
+    process.on('SIGTERM', onShutdown)
 })()
